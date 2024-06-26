e63b19a3ba7dbdf106acbcc6c2d0e932103ee069
Antonio Torres Mora <anttorre@c2r2s2.42malaga.com>
2024-06-26 15:04:24 +0200

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,26 @@
 /*   By: anttorre <anttorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 12:57:25 by anttorre          #+#    #+#             */
-/*   Updated: 2024/06/24 15:46:39 by anttorre         ###   ########.fr       */
+/*   Updated: 2024/06/26 14:50:16 by anttorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 int main()
 {
-	Bureaucrat *b1 = new Bureaucrat("Antonio", 150);
+	Bureaucrat *b1 = new Bureaucrat("Antonio", 145);
 	Bureaucrat *b2 = new Bureaucrat("Juan", 1);
 
 	std::cout << std::endl;
 
 	std::cout << *b1 << "\n" << *b2 << std::endl;
+	b1->decrement();
+	b2->decrement();
+	std::cout << *b1 << "\n" << *b2 << std::endl;
+	b1->increment();
+	b2->increment();
+	std::cout << *b1 << "\n" << *b2 << std::endl;
+	b2->increment();
 
 	std::cout << std::endl;
 
